e4721931a5cc94033e94a8f71cb95f0bc8045b3c
Antoine Grondin <antoinegrondin@gmail.com>
2016-02-26 17:08:40 +0530

    listen on all interfaces, assign floating IP

--- deploy/main.tf
+++ deploy/main.tf
@@ -46,3 +46,8 @@ systemctl start web.service
 CMD
     }
 }
+
+resource "digitalocean_floating_ip" "45.55.99.29" {
+  droplet_id = "${digitalocean_droplet.web.id}"
+  region = "${digitalocean_droplet.web.region}"
+}
