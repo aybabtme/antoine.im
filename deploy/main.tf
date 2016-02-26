a581114209fdcdf1a642baa539b68821ccb2add2
Antoine Grondin <antoinegrondin@gmail.com>
2016-02-26 17:41:42 +0530

    fix prometheus watch

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,15 +1,18 @@
 provider "digitalocean" {}
 
-variable "region"   {}
-variable "web_size" {}
 variable "ssh_keys" {}
-variable "brog_version" {default="0.3"}
+
+variable "region"       { default="nyc3" }
+variable "web_size"     { default="4gb" }
+variable "metrics_size" { default="1gb" }
+variable "brog_version" { default="0.3" }
+variable "prom_version" { default="0.17.0rc2" }
 
 resource "digitalocean_droplet" "web" {
     count = 1
 
     image              = "debian-8-x64"
-    name               = "web${count.index}.aybabt.me.debian"
+    name               = "web${count.index}.aybabt.me"
     region             = "${var.region}"
     size               = "${var.web_size}"
     ssh_keys           = ["${split(",", var.ssh_keys)}"]
@@ -17,7 +20,7 @@ resource "digitalocean_droplet" "web" {
 
     provisioner "remote-exec" {
             inline = <<CMD
-apt-get update && apt-get upgrade -y git
+apt-get install -y git
 
 mkdir /srv/
 git clone https://github.com/aybabtme/antoine.im.git /srv/aybabt.me
@@ -47,7 +50,65 @@ CMD
     }
 }
 
-resource "digitalocean_floating_ip" "45.55.99.29" {
-  droplet_id = "${digitalocean_droplet.web.id}"
-  region = "${digitalocean_droplet.web.region}"
+
+resource "digitalocean_droplet" "prometheus" {
+    image              = "debian-8-x64"
+    name               = "prometheus.aybabt.me"
+    region             = "${var.region}"
+    size               = "${var.metrics_size}"
+    ssh_keys           = ["${split(",", var.ssh_keys)}"]
+    private_networking = true
+
+    provisioner "remote-exec" {
+            inline = <<CMD
+mkdir -p /opt/bin/prometheus/
+wget -qO- https://github.com/prometheus/prometheus/releases/download/${var.prom_version}/prometheus-${var.prom_version}.linux-amd64.tar.gz | tar xvz
+cd prometheus-*
+mv ./prometheus /opt/bin/prometheus/prometheus
+
+mkdir -p /var/lib/prometheus
+mkdir -p /etc/prometheus
+
+cat > /etc/systemd/system/prometheus.service <<EOF
+[Unit]
+Description=prometheus
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/opt/bin/prometheus/prometheus -config.file=/etc/prometheus/config.yml -storage.local.path=/var/lib/prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/prometheus/config.yml <<EOF
+global:
+  scrape_interval:     2s
+  evaluation_interval: 15s
+
+scrape_configs:
+  - job_name: 'aybabtme'
+
+    scrape_interval: 2s
+    scrape_timeout: 10s
+    metrics_path: /debug/metrics
+
+    target_groups:
+      - targets: ['${digitalocean_droplet.web.ipv4_address_private}:80']
+
+  - job_name: 'prometheus'
+
+    scrape_interval: 5s
+    scrape_timeout: 10s
+
+    target_groups:
+      - targets: ['localhost:9090']
+EOF
+
+systemctl enable prometheus.service
+systemctl start prometheus.service
+CMD
+    }
 }
